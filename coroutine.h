dd6f19141c25536028aa8e582cea4cb408e91883
HubbardHuang <hubbard_huang@sina.com>
2020-03-14 22:42:21 +0800

    refactor(coroutine): Add access control for class `Coroutine`

--- coroutine.h
+++ coroutine.h
@@ -22,11 +22,11 @@ struct Context {
 #define STACK_SIZE 1024
 
 class Coroutine {
-public:
-    std::shared_ptr<CoroutineEnvironment> env_;
-    bool can_run_next_time_;
+    friend std::shared_ptr<CoroutineEnvironment> CurrEnv(void);
 
 private:
+    std::shared_ptr<CoroutineEnvironment> env_;
+    bool can_run_next_time_;
     Context context_;
     char stack_[STACK_SIZE];
 
